8a317ccd0ddac70b863c0d1fc64b32f3003ad8bf
roman <ronnoverro@gmail.com>
2023-04-26 19:53:48 -0700

    learning ab. aspect ratios

--- source/Main.hx
+++ source/Main.hx
@@ -2,7 +2,6 @@ package;
 
 import flixel.FlxG;
 import flixel.FlxGame;
-import flixel.FlxGame;
 import flixel.ui.FlxButton;
 import flixel.util.FlxSave;
 import openfl.display.Sprite;
@@ -14,6 +13,8 @@ class Main extends Sprite
 	{
 		super();
 
+		// PARAMS: new(gameWidth:Int = 0, gameHeight:Int = 0, ?initialState:Class<FlxState>, updateFramerate:Int = 60,
+		// drawFramerate:Int = 60, skipSplash:Bool = false, startFullscreen:Bool = false)
 		addChild(new FlxGame(320, 240, MenuState, false)); // what does false do again?
 	}
 }
--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,6 +1,12 @@
 package;
 
+import flixel.FlxG;
 import flixel.FlxState;
+import flixel.group.FlxGroup.FlxTypedGroup;
+import flixel.system.FlxSound;
+import flixel.text.FlxText;
+import flixel.tile.FlxTilemap;
+import flixel.util.FlxColor;
 
 class PlayState extends FlxState
 {
@@ -12,7 +18,7 @@ class PlayState extends FlxState
 	override public function update(elapsed:Float)
 	{
 		super.update(elapsed);
-		var text = new flixel.text.FlxText(0, 0, 0, "Hello World", 64);
+		var text = new flixel.text.FlxText(0, 0, 0, "Hello World", 32);
 		text.screenCenter();
 		add(text);
 	}
