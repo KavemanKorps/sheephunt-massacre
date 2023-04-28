8e046d4e38c57759ad2aedc2a5afa2a822a53acc
roman <ronnoverro@gmail.com>
2023-04-28 16:41:57 -0700

    gameOver state added

---
+++ GameOver.hx
@@ -0,0 +1,48 @@
+package;
+
+import flixel.FlxG;
+import flixel.FlxState;
+import flixel.text.FlxText;
+import flixel.ui.FlxButton;
+import flixel.util.FlxAxes;
+import flixel.util.FlxColor;
+
+class GameOverState extends FlxState
+{
+	var win:Bool; // if we won or lost
+	var titleText:FlxText; // the title text
+	var mainMenuButton:FlxButton; // button to go to main menu
+
+	public function new(win:Bool, score:Int)
+	{
+		super();
+		this.win = win;
+	}
+
+	override public function create()
+	{
+		#if FLX_MOUSE
+		FlxG.mouse.visible = true;
+		#end
+
+		titleText = new FlxText(0, 20, 0, if (win) "You Win!" else "Game Over!", 22);
+		titleText.alignment = CENTER;
+		titleText.screenCenter(FlxAxes.X);
+		add(titleText);
+
+		mainMenuButton = new FlxButton(0, FlxG.height - 32, "Main Menu", switchToMainMenu);
+		mainMenuButton.screenCenter(FlxAxes.X);
+		add(mainMenuButton);
+
+		super.create();
+	}
+
+	function switchToMainMenu():Void // void func: it returns nothing.
+	{
+		// anonymous callback func. that switches to MenuState() after done fading to black
+		FlxG.camera.fade(FlxColor.BLACK, 0.33, false, function()
+		{
+			FlxG.switchState(new MenuState());
+		});
+	}
+}
